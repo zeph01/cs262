a54040660a070e90c9985fdd68ce2ea6ba3dc914
Belina <belina3234@gmail.com>
2020-10-10 22:32:22 -0400

    adding lab06 again

---
+++ lab06/myproject/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=eckhart%20tolle')
+      .then((response) => response.json())
+      .then((json) => setData(json.items))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.volumeInfo.title}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
